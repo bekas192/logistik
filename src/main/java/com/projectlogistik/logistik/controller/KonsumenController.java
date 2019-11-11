fbc967f2a5ed1662c0e04684b9050d2bc11d8008
arrif <arrif@DESKTOP-EFS6P8F>
2019-11-11 16:07:29 +0700

    ganti port

--- src/main/java/com/projectlogistik/logistik/controller/KonsumenController.java
+++ src/main/java/com/projectlogistik/logistik/controller/KonsumenController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author 300
  */
-@RequestMapping("/sevlog/konsumen")
+@RequestMapping("/konsumen")
 @RestController
 public class KonsumenController {
     private KonsumenService konsumenService;
